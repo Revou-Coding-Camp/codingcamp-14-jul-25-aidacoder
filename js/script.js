c104524fb41cea7c495096db8055e41ba5e7d169
aidacoder <marhalidasinaga12@gmail.com>
2025-07-19 11:09:25 +0700

    gender fix

--- js/script.js
+++ js/script.js
@@ -7,13 +7,18 @@ window.addEventListener('DOMContentLoaded', function() {
         welcomeUserElement.textContent = 'Guest';
     }
 
+    document.getElementById('name-input').value = initialName; 
+
 
 })
 
 function validateForm() { 
     const name = document.getElementById('name-input');
     const dob = document.getElementById("dob-input");
+    const male = document.getElementById("male");
+    const female = document.getElementById("female");
     const message = document.getElementById("message-input");
+    let finalGender = '';
     
 
     // Validation messages
@@ -24,12 +29,12 @@ function validateForm() {
         name.value = ''; // Clear the input field after submission
     }
 
-    if (dob.value === '') {
-        alert('Please enter your  date.');
-    } else {
-        document.getElementById('display-dob').innerHTML =  `Tempat tgl,lahir: ${name.value}`;
+ 
+        document.getElementById('display-dob').innerHTML =  `Tempat tgl,lahir: ${dob.value}`;
         dob.value = ''; // Clear the input field after submission
-    }
+    
+
+
 
 
     if (message.value === '') {
@@ -39,6 +44,14 @@ function validateForm() {
         message.value = ''; // Clear the input field after submission
     }
 
+    if(male.checked) {
+        finalGender = "Males"
+    }else if (female.checked) {
+        finalGender = "Females"
+    } else {
+        finalGender = "Not specified"
+    }
+    document.getElementById('display-gender').innerHTML = `Gender : ${finalGender}`
     
 }
 
